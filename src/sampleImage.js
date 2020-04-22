95ea068ae1b7661e4ddb8a15af8e2ce0c65c1b86
Dorairaju <dorairaj512@gmail.com>
2020-04-22 05:32:25 -0500

    integrate express js to serve static pages

--- src/sampleImage.js
+++ src/sampleImage.js
@@ -1,6 +1,5 @@
 import Heading from './components/heading/heading.js';
 import AddImage from './components/sample-image/addImage.js';
-import React from 'react';
 
 const heading = new Heading();
 const addImage = new AddImage();
---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.get('/hello-world/', function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+  res.send(contentFromHtmlFile);
+});
+
+app.get('/image/', function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/sampleImage.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+  res.send(contentFromHtmlFile);
+});
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')));
+app.listen(3000, function() {
+  console.log('Application is running on port 3000');
+});
